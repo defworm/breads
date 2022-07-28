b3b7b42a9c050578a4c3f262ed3fec91d9af8342
Crystal Korth <crystalkorth@gmail.com>
2022-07-27 20:08:17 -0500

    lesson 3 about to add data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,13 +4,18 @@ const Bread = require ('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send('This is the index at /breads')
+    res.render('index')
+  // res.send(Bread)
 })
+
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     res.send(Bread[req.params.arrayIndex])
   })
+  
+
   
 
 module.exports = breads
