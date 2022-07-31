5aedd20dc6d4c5e34794cb71ca5c7bc4defccce1
Crystal Korth <crystalkorth@gmail.com>
2022-07-31 13:47:42 -0500

    finished Code Along (be-4-ssr-views-show-and-adding-css)

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -25,6 +25,17 @@ breads.get('/:arrayIndex', (req, res) => {
       res.send('404')
     }
   })
+
+  breads.get('/:arrayIndex', (req, res) => {
+    if (Bread[req.params.arrayIndex]) {
+      res.render('Show', {
+        bread:Bread[req.params.arrayIndex]
+      })
+    } else {
+      res.render('404')
+    }
+  })
+  
   
   
   
