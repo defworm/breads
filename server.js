a8eaad13e3c1c4be45c9ef7c18b324f3d93eb32e
Crystal Korth <crystalkorth@gmail.com>
2022-08-03 19:14:22 -0500

    testing

--- server.js
+++ server.js
@@ -1,45 +1,45 @@
 
 
 
-// DEPENDENCIES
-const express = require('express')
-const methodOverride = require('method-override')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const methodOverride = require("method-override");
+const mongoose = require("mongoose");
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+
+const PORT = process.env.PORT;
+const app = express();
 
 // MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride ('_method'))
-
-//Mongoose
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-  )
-  
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
-  
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
+app.get("/", (req, res) => {
+  res.send("Welcome to an awesome App about Breads");
+});
+
+// Breads
+const breadsController = require("./controllers/breads_controller.js");
+app.use("/breads", breadsController);
+
 // 404 Page
-app.get('*', (req, res) => {
-    res.send('404')
-  })
-  
+app.get("*", (req, res) => {
+  res.send("404");
+});
+
 // LISTEN
 app.listen(PORT, () => {
-  console.log('listening on port', PORT);
-})
+  console.log("nomming at port", PORT);
+});
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
